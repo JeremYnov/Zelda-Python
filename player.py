ad6e5c040170f680a92baa15b073d9d6b2749193
OwainCharlon <45096017+OwainCharlon@users.noreply.github.com>
2020-01-27 14:14:24 +0100

    deja dans le ressources

--- player.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-# Définit notre classs comme une entité du jeu dans pygame
-class Player(pygame.sprite.Sprite,Entity) :
-    """Classe définissant notre personnage principal"""
-    def __init__(self):
-        self.image = pygame.image.load('chemin/nomdelimage')
-        self.rect = self.image.get_rect()
